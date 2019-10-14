28a8f232e6a5eb20eebe9c9f24c9f7cc867762e3
Daniela Mansilla <daniela_mansilla94@hotmail.com>
2019-10-13 21:08:59 -0300

    Ya va a crear el evento, la redireccion de la vista nada mas

--- Vita/Controllers/EventoController.cs
+++ Vita/Controllers/EventoController.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vita.Servicios;
 
 namespace Vita.Controllers
 {
     public class EventoController: Controller
     {
-        
+        private UsuarioServicio usuarioServicio = new UsuarioServicio();
+        private CategoriaServicio categoriaServicio = new CategoriaServicio();
+        private SexoServicio sexoServicio = new SexoServicio();
+        private SegmentoServicio segmentoServicio = new SegmentoServicio();
+        private LocalidadServicio localidadServicio = new LocalidadServicio();
+        private VitaEntities myDbContext = new VitaEntities();
+
+        [HttpGet]
         public ActionResult CrearEvento()
         {
-            return View();
+            var buscarUsuarioLogueado = Session["Usuario"] as Usuario; //obtengo usuario logueado
+            if(buscarUsuarioLogueado == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else
+            {
+                buscarUsuarioLogueado = usuarioServicio.GetUsuarioById(buscarUsuarioLogueado.Id);
+                return View(buscarUsuarioLogueado);
+
+            }
         }
+        [HttpPost]
+        public ActionResult CreacionEvento(Evento evento)
+        {
+            //obtengo usuario logueado
+            if (!(Session["Usuario"] is Usuario buscarUsuarioLogueado))
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else
+            {
+                buscarUsuarioLogueado = usuarioServicio.GetUsuarioById(buscarUsuarioLogueado.Id);
+                return RedirectToAction("PerfilEntidad", "Usuario", buscarUsuarioLogueado);
+
+            }
+        }
+
+
         public ActionResult Eventos()
         {
             return View();
