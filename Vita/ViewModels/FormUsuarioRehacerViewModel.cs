8e9fa6ed2de800bfa6f806825a740eb10307c7f7
Daniela Mansilla <daniela_mansilla94@hotmail.com>
2019-12-03 06:19:04 -0300

    Se  sube los motivos por el cual se rehace el formulario

---
+++ Vita/ViewModels/FormUsuarioRehacerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vita.ViewModels
+{
+    public class FormUsuarioRehacerViewModel
+    {
+            public List<bool> Rehacer { get; set; }
+
+            public List<Respuesta> Respuestas { get; set; }
+
+
+        }
+    }
